0027b8658729128cd614e2117488198c106b6ac5
Germán Ponce Dominguez <german.ponce@argil.mx>
2022-06-14 22:21:13 -0500

    Correcciones

--- models/res_users_inherit.py
+++ models/res_users_inherit.py
@@ -39,9 +39,9 @@ class ResPartner(models.Model):
     def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
 
         if self.env.user.dont_show_suppliers:
-            args.append(('customer_rank', '=', 0))
-        if self.env.user.dont_show_customers:
             args.append(('supplier_rank', '=', 0))
+        if self.env.user.dont_show_customers:
+            args.append(('customer_rank', '=', 0))
         res = super(ResPartner, self)._search(args, offset=offset, limit=limit,
                                                     order=order, count=count, access_rights_uid=access_rights_uid)
 
